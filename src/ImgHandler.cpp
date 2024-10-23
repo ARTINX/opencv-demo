bf97b4217b08786163ef6e0aee130c1f9bba1559
zitong <12012710@mail.sustech.edu.cn>
2024-10-24 02:28:32 +0800

    fix(ImgHandler.cpp): add const to member function

--- src/ImgHandler.cpp
+++ src/ImgHandler.cpp
@@ -1,10 +1,10 @@
 #include "ImgHandler.hpp"
 
-void ImageHandler::readImg(const std::string& path, cv::Mat& result){
+void ImageHandler::readImg(const std::string& path, cv::Mat& result) const{
     result = cv::imread(path, cv::IMREAD_COLOR);
 }
 
-void ImageHandler::showImg(const cv::Mat& img, const std::string& windowName){
+void ImageHandler::showImg(const cv::Mat& img, const std::string& windowName) const{
     cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
     cv::imshow(windowName, img);
     cv::waitKey(0);
