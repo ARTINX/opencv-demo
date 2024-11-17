e2e3502702b52848a8e7ea7ed565ad2fb6cefd71
huangzitong <12432670@mail.sustech.edu.cn>
2024-11-17 22:01:16 +0800

    feat(utils): add logInfo function

---
+++ include/utils.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string_view>
+
+void logInfo(const std::string_view& msg);
---
+++ src/utils.cpp
@@ -0,0 +1,6 @@
+#include "utils.hpp"
+#include <iostream>
+
+void logInfo(const std::string_view& msg) {
+    std::cout << msg << std::endl;
+}
