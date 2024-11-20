69947ff85ad1dc3eab88d732e5e2ce8cfe2c51c3
Zeng Zishan <12310733@mail.sustech.edu.cn>
2024-11-20 12:01:37 +0800

    docs: 添加代码注释

--- src/HikDriver.cpp
+++ src/HikDriver.cpp
@@ -18,6 +18,16 @@
         }                                                                                     \
     }
 
+/**
+ * @brief Constructs a HikDriver object.
+ *
+ * Initializes the HikDriver with the provided image callback and configuration path.
+ * Displays the driver version, reads the configuration file, and opens the camera.
+ * If reading the config file fails, logs a warning and sets default exposure time and gain.
+ *
+ * @param imageCallBack A callback function to process images.
+ * @param configPath The path to the configuration file.
+ */
 HikDriver::HikDriver(std::function<void(cv::Mat&)>& imageCallBack, const std::string& configPath) {
     this->imageCallBack = imageCallBack;
     showDriverVersion();
@@ -35,7 +45,12 @@ HikDriver::HikDriver(std::function<void(cv::Mat&)>& imageCallBack, const std::st
 HikDriver::~HikDriver() {
     closeCamera();
 }
-
+/**
+ * @brief read Camera settings from configuration file
+ * 
+ * @param filePath the file path of the configuration file
+ * 
+ */
 void HikDriver::readConfig(const std::string& filePath) {
     std::ifstream configFile(filePath);
     std::map<std::string, std::string> configMap;
@@ -67,14 +82,29 @@ void HikDriver::readConfig(const std::string& filePath) {
     this->exposureTime = std::stod(configMap["exposureTime"]);
     this->gain = std::stod(configMap["gain"]);
 }
-
+/**
+ * @brief show the Hik driver version
+ * 
+ */
 void HikDriver::showDriverVersion() {
     auto version = MV_CC_GetSDKVersion();
     logInfo(fmt::format("Hik driver version: {}.{}.{}.{}", (version & 0xFF000000) >> 24, (version & 0xFF0000) >> 16,
                         (version & 0xFF00) >> 8, version & 0xFF));
 }
-
+/**
+ * @brief Opens the Hik camera with specified retry parameters.
+ *
+ * This function attempts to find and open a single Hik camera device. It retries the connection
+ * for a specified number of times with a given interval between retries. Upon successful
+ * detection, it initializes the camera handle, configures various camera settings such as
+ * exposure, gain, acquisition mode, and balance white auto. It also loads calibration data
+ * and starts image grabbing with a BGR callback.
+ *
+ * @param retryTimes The number of times to retry finding the camera if none are detected.
+ * @param retryInterval The interval between each retry attempt.
+ */
 void HikDriver::openCamera(uint32_t retryTimes, Duration retryInterval) {
+    // Enumerate devices until at least one is found
     for(uint32_t i = 0;; ++i) {
         CheckErrorCode(MV_CC_EnumDevices(MV_USB_DEVICE, &mDeviceList));
         if(mDeviceList.nDeviceNum > 0) {
@@ -87,14 +117,26 @@ void HikDriver::openCamera(uint32_t retryTimes, Duration retryInterval) {
         std::this_thread::sleep_for(retryInterval);
     }
     logInfo(fmt::format("{} camera(s) found", mDeviceList.nDeviceNum));
+    
+    // Open the first device found
     mDeviceInfo = mDeviceList.pDeviceInfo[0];
+    
+    // Get camera serial number
     mCameraSerialNumber = std::string(reinterpret_cast<char*>(mDeviceInfo->SpecialInfo.stUsb3VInfo.chSerialNumber));
+    
+    // Create camera handle
     CheckErrorCode(MV_CC_CreateHandleWithoutLog(&mCameraHandle, mDeviceInfo));
+    
+    // Don't mention. It's try to solve a bug in the driver. 
     CheckErrorCode(MV_CC_CloseDevice(mCameraHandle));
+
     CheckErrorCode(MV_CC_OpenDevice(mCameraHandle, MV_ACCESS_Control));
     CheckErrorCode(MV_CC_GetImageInfo(mCameraHandle, &mImageInfo));
     logInfo(fmt::format("Resolution for {}: {} x {}", mCameraSerialNumber, mImageInfo.nWidthMax, mImageInfo.nHeightMax));
+    
     loadCalibration(mCameraSerialNumber, mImageInfo.nWidthMax, mImageInfo.nHeightMax);
+    
+    // Set the properties of Camera, including Exposure time, Gain, AcquisitionMode, BalanceWhite...
     CheckErrorCode(MV_CC_SetEnumValue(mCameraHandle, "ExposureAuto", MV_EXPOSURE_AUTO_MODE_OFF));
     CheckErrorCode(MV_CC_SetEnumValue(mCameraHandle, "ExposureMode", MV_EXPOSURE_MODE_TIMED));
     CheckErrorCode(MV_CC_SetFloatValue(mCameraHandle, "ExposureTime", this->exposureTime * 1.0e6));
@@ -103,15 +145,35 @@ void HikDriver::openCamera(uint32_t retryTimes, Duration retryInterval) {
     CheckErrorCode(MV_CC_SetEnumValue(mCameraHandle, "BalanceWhiteAuto", MV_BALANCEWHITE_AUTO_OFF));
     CheckErrorCode(MV_CC_SetBayerCvtQuality(mCameraHandle, 1));
     CheckErrorCode(MV_CC_RegisterImageCallBackForBGR(mCameraHandle, newFrame, this));
+
+    // Start grabbing images from the camera
     CheckErrorCode(MV_CC_StartGrabbing(mCameraHandle));
 }
 
+/**
+ * @brief Handles a new frame received by the HikDriver.
+ *
+ * This function is called when a new frame is available. It converts the raw frame data into a cv::Mat object
+ * in BGR format and invokes the image callback with the processed image.
+ *
+ * @param pData Pointer to the raw frame data.
+ * @param pFrameInfo Pointer to the frame information structure containing metadata about the frame.
+ * @param pUser Pointer to the user-defined data, typically a reference to the HikDriver instance.
+ */
 void HikDriver::newFrame(unsigned char* pData, MV_FRAME_OUT_INFO_EX* pFrameInfo, void* pUser) {
     auto driver = static_cast<HikDriver*>(pUser);
     cv::Mat bgr{ pFrameInfo->nHeight, pFrameInfo->nWidth, CV_8UC3, static_cast<void*>(pData) };
     driver->imageCallBack(bgr);
 }
 
+/**
+ * @brief Loads camera calibration data for the specified identifier.
+ *
+ * @param identifier A unique string identifier for the calibration data (e.g., serial number).
+ * @param width The width of the camera image in pixels.
+ * @param height The height of the camera image in pixels.
+ * @param fallbackFov The fallback field of view (in degrees) to use if calibration data is unavailable.
+ */
 void HikDriver::loadCalibration(const std::string& identifier, const uint32_t width, const uint32_t height,
                                 const double fallbackFov) {
     const auto inputFileName = "./calibration/" + identifier + ".xml";
@@ -129,7 +191,10 @@ void HikDriver::loadCalibration(const std::string& identifier, const uint32_t wi
         this->distCoefficients = cv::Mat{};
     }
 }
-
+/**
+ * @brief Close the camera dirver
+ * 
+ */
 void HikDriver::closeCamera() {
     CheckErrorCode(MV_CC_StopGrabbing(mCameraHandle));
     CheckErrorCode(MV_CC_CloseDevice(mCameraHandle));
--- src/ImgHandler.cpp
+++ src/ImgHandler.cpp
@@ -1,9 +1,26 @@
 #include "ImgHandler.hpp"
 
+/**
+ * @brief Reads an image from the specified file path.
+ *
+ * This function loads an image from the given file path and stores it in the provided cv::Mat object.
+ *
+ * @param path The file path to the image.
+ * @param result A reference to a cv::Mat object where the loaded image will be stored.
+ */
 void ImageHandler::readImg(const std::string& path, cv::Mat& result) const{
     result = cv::imread(path, cv::IMREAD_COLOR);
 }
 
+/**
+ * @brief Displays the provided image in a window with the specified name.
+ *
+ * This function creates a window with the given name, displays the provided cv::Mat image,
+ * and waits for a key press to close the window.
+ *
+ * @param img The image to be displayed.
+ * @param windowName The name of the window in which the image will be shown.
+ */
 void ImageHandler::showImg(const cv::Mat& img, const std::string& windowName) const{
     cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
     cv::imshow(windowName, img);
